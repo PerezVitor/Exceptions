86aae4fb944b46f27ad5f7c653448b0efba8b508
Vitor Perez <vitor.g.perez@hotmail.com>
2018-12-24 15:30:45 -0200

    Finished project

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Course.Entities;
+using Course.Entities.Exceptions;
+using System.Globalization;
 
 namespace Course
 {
@@ -10,6 +9,36 @@ namespace Course
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Enter the account data:");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw Limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+                Console.Write("\nEnter amount for withdraw: ");
+                double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                acc.WithDraw(withdraw);
+                Console.WriteLine("New balance: $" + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Withdraw error : " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
         }
     }
 }
